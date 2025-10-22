41f5725d9452aa02617a0e5af9fd1fb622ccfe49
samlee-pub <samlee_pub@sina.com>
2025-10-22 15:36:39 +0800

    Delete chapter-04/react-component-app/src directory

--- chapter-04/react-component-app/src/ChildFC.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * @ReactDOM Component: <ChildFC message={"..."} />
- * @param {string} props.message - The message sent from the parent component.
- * @example
- * <ChildFC message="Hello from ChildFC!" />
- */
-export function ChildFC({ message }: { message: string }) {
-  return <div>Child says: {message}</div>;
-}
