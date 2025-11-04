0fe60c345426b5c626598d5a92359480fdaa2266
samlee-pub <samlee_pub@sina.com>
2025-11-04 09:43:38 +0800

    Delete chapter-09/component-navigate-react-router directory

--- chapter-09/component-navigate-react-router/src/suspense-routing/LazyPage.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * This component is a simple component that displays a heading.
- * It is used to demonstrate the lazy loading of components.
- */
-export default function LazyPage() {
-  return <h2 className="App">Lazy Page</h2>;
-}
